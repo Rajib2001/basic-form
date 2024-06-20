8e6debea9dff10e676e2a015cbc28b070b33fad8
Rabiya <91591634+Rajib2001@users.noreply.github.com>
2024-06-21 01:37:29 +0530

    useEffect and useState are removed

--- src/FormComponent/EventRegistrationForm.js
+++ src/FormComponent/EventRegistrationForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useForm from './useForm'; // Custom hook for form handling and validation
 
